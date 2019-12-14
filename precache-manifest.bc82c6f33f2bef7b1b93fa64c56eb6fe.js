3e601d5216575ca1e03ba5e67dcc1dbcd5aa4ff5
Vuyani Shabangu <vuyani.shabangu@gmail.com>
2019-12-14 15:43:13 +0200

    Updates

---
+++ precache-manifest.bc82c6f33f2bef7b1b93fa64c56eb6fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "43e7628369b34b1a73f233e04a0dbc3c",
+    "url": "/static/media/shape.43e76283.svg"
+  },
+  {
+    "revision": "89d61a1389450c2d2370ccd1c49aab69",
+    "url": "/static/media/nl-logo.89d61a13.png"
+  },
+  {
+    "revision": "c1b3931e3dc1c882da4219ef76092d9a",
+    "url": "/static/media/mainBackground.c1b3931e.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab13db6f7036bcc8332f",
+    "url": "/static/js/main.ab13db6f.chunk.js"
+  },
+  {
+    "revision": "d26223375905746e58ac",
+    "url": "/static/js/1.d2622337.chunk.js"
+  },
+  {
+    "revision": "ab13db6f7036bcc8332f",
+    "url": "/static/css/main.f849f0e6.chunk.css"
+  },
+  {
+    "revision": "e33b8247184eaa80c82bf56fc329d4f3",
+    "url": "/index.html"
+  }
+];
